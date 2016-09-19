884b68b4a0430d682943e0df419174591a86d48b
Elliott Zwertvaegher <Elliott Zwertvaegher>
2016-09-19 22:42:13 +0900

    Initial commit

---
+++ CerealSquad/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace CerealSquad
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            RenderWindow win = new RenderWindow(new VideoMode(800, 600), "[DEV] Cereal Squad");
+#else
+            RenderWindow win = new RenderWindow(new VideoMode(800, 600), "[PROD] Cereal Squad");
+#endif
+            win.Closed += Win_Closed;
+
+            while (win.IsOpen)
+            {
+                win.DispatchEvents();
+                win.Clear();
+                win.Display();
+            }
+        }
+
+        private static void Win_Closed(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+        }
+    }
+}
