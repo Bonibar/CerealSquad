be977b88f885ea628421d644e97319eb4a07a564
Pierre <pierre.noel@epitech.eu>
2016-10-03 09:33:22 +0200

    Simple handling event

--- CerealSquad/Program.cs
+++ CerealSquad/Program.cs
@@ -12,30 +12,10 @@ namespace CerealSquad
         /// </summary>
         static void Main()
         {
-#if DEBUG
-            RenderWindow win = new RenderWindow(new VideoMode(800, 600), "[DEV] Cereal Squad");
-#else
-            RenderWindow win;
-            if (VideoMode.FullscreenModes.Length > 0)
-                win = new RenderWindow(VideoMode.FullscreenModes[0], "[PROD] Cereal Squad");
-            else
-                win = new RenderWindow(new VideoMode(800, 600), "[PROD] Cereal Squad");
-#endif
-            win.Closed += Win_Closed;
+            Renderer renderer = new Renderer();
 
-
-            while (win.IsOpen)
-            {
-                win.DispatchEvents();
-                win.Clear(Color.Magenta);
-                win.Display();
-            }
-        }
-
-        private static void Win_Closed(object sender, EventArgs e)
-        {
-            RenderWindow window = (RenderWindow)sender;
-            window.Close();
+            renderer.initialization();
+            renderer.loop();
         }
     }
 }
---
+++ CerealSquad/WindowsEventHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace CerealSquad
+{
+    class WindowsEventHandler
+    {
+        private RenderWindow renderWindow;
+
+        public WindowsEventHandler(RenderWindow _renderWindow)
+        {
+            renderWindow = _renderWindow;
+
+            renderWindow.Closed += RenderWindow_Closed;
+            renderWindow.Resized += RenderWindow_Resized;
+            renderWindow.LostFocus += RenderWindow_LostFocus;
+            renderWindow.GainedFocus += RenderWindow_GainedFocus;
+            renderWindow.KeyPressed += RenderWindow_KeyPressed;
+            renderWindow.KeyReleased += RenderWindow_KeyReleased;
+            
+        }
+
+        private void RenderWindow_Resized(object sender, SizeEventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+        }
+
+        private void RenderWindow_GainedFocus(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+        }
+
+        private void RenderWindow_KeyReleased(object sender, KeyEventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+        }
+
+        private void RenderWindow_KeyPressed(object sender, KeyEventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            
+            if (e.Code == Keyboard.Key.Escape) {
+                window.Close();
+            }
+        }
+
+        private void RenderWindow_Closed(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+        }
+
+        private void RenderWindow_LostFocus(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+        }
+    }
+}
