f79e8bc71ce51a13b4dcb639574f893c2d2dddb8
Elliott Zwertvaegher <Elliott Zwertvaegher>
2016-10-24 14:13:56 +0900

    Displaying text on window

---
+++ CerealSquad/AMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerealSquad
+{
+    public class AMenu
+    {
+        protected SFML.Graphics.RenderWindow _Win;
+        protected InputManager _InputManager;
+
+        public AMenu(SFML.Graphics.RenderWindow win, InputManager inputManager)
+        {
+            _InputManager = inputManager;
+            _Win = win;
+        }
+
+        private bool _displayed = false;
+        public bool Displayed { get { return _displayed; } }
+
+        SFML.Graphics.Text back = new SFML.Graphics.Text("Back", new SFML.Graphics.Font("Fonts/xirod.regular.ttf"));
+
+        public void Show()
+        {
+            if (!Displayed)
+            {
+                _displayed = true;
+            }
+        }
+
+        public void Hide()
+        {
+            if (Displayed)
+            {
+                _displayed = false;
+            }
+        }
+
+        public void Toggle()
+        {
+            if (Displayed)
+                Hide();
+            else
+                Show();
+        }
+
+        public void update()
+        {
+            if (Displayed)
+            {
+                
+                _Win.Draw(back);
+            }
+        }
+    }
+}
--- CerealSquad/Program.cs
+++ CerealSquad/Program.cs
@@ -12,10 +12,38 @@ namespace CerealSquad
         /// </summary>
         static void Main()
         {
+            /*
             Renderer renderer = new Renderer();
 
             renderer.initialization();
             renderer.loop();
+            */
+            RenderWindow win = new RenderWindow(new VideoMode(800, 800), "Cereal Menu");
+            InputManager manager = new InputManager(win);
+            AMenu mainMenu = new AMenu(win, manager);
+
+            manager.KeyboardKeyPressed += Manager_KeyboardKeyPressed;
+
+            mainMenu.Show();
+
+            while (win.IsOpen)
+            {
+                win.DispatchEvents();
+                win.Clear(Color.Magenta);
+                if (mainMenu.Displayed)
+                    mainMenu.update();
+                else
+                {
+                    // GameLogic
+                }
+                win.Display();
+            }
+        }
+
+        private static void Manager_KeyboardKeyPressed(object source, Keyboard.KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keyboard.Key.Escape))
+                ((RenderWindow)source).Close();
         }
     }
 }
