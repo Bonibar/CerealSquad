673f3bfceece4d6146451ad9247c319523bf5ceb
dubell_g <dubell_g@epitech.eu>
2016-10-27 10:56:11 +0900

    Implementation of Game, World and Rooms classes (without entities)

---
+++ CerealSquad/AWorld.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerealSquad
+{
+    class AWorld
+    {
+        protected List<ARoom> Rooms = new List<ARoom>();
+
+        public List<ARoom> getRooms()
+        {
+            return (Rooms);
+        }
+
+        public void DispRooms()
+        {
+            foreach (ARoom Room in Rooms)
+            {
+                Console.WriteLine("RoomType = " + Room.getRoomType());
+            }
+        }
+    }
+}
