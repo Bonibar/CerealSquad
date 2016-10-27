c1b59c991d910b1f5ab71dad9f58f4456a603fb3
Pierre <pierre.noel@epitech.eu>
2016-10-27 07:49:02 +0200

    Add FrameClock class

---
+++ CerealSquad/FrameClock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+
+namespace CerealSquad
+{
+    class FrameClock
+    {
+        Clock frameClock = new Clock();
+
+        public Time Restart()
+        {
+           return frameClock.Restart();
+        }
+    }
+}
