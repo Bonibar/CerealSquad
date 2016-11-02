dee45c30f2b7fbc7d7e7e19d278a302840cdae62
dubell_g <dubell_g@epitech.eu>
2016-11-03 00:03:44 +0900

    Fixed Draw for AWorld and ARoom

--- CerealSquad/GameWorld/AWorld.cs
+++ CerealSquad/GameWorld/AWorld.cs
@@ -9,7 +9,6 @@ namespace CerealSquad.GameWorld
 {
     class AWorld : Drawable
     {
-        public static uint TILE_SIZE = 64;
         protected List<ARoom> Rooms = new List<ARoom>();
 
         public void AddRoom(ARoom room)
@@ -21,14 +20,15 @@ namespace CerealSquad.GameWorld
         public void Draw(RenderTarget target, RenderStates states)
         {
             Rooms.ForEach((ARoom room) => {
-                RenderTexture roomTexture = new RenderTexture(room.Size.Width * TILE_SIZE, room.Size.Height * TILE_SIZE);
+                /*RenderTexture roomTexture = new RenderTexture(room.Size.Width * TILE_SIZE, room.Size.Height * TILE_SIZE);
                 room.Draw(roomTexture, states);
                 Sprite roomSprite = new Sprite(roomTexture.Texture);
 
                 roomSprite.Position = new SFML.System.Vector2f(room.Position.X * TILE_SIZE, room.Position.Y * TILE_SIZE);
                 target.Draw(roomSprite, states);
                 roomSprite.Dispose();
-                roomTexture.Dispose();
+                roomTexture.Dispose();*/
+                target.Draw(room, states);
             });
         }
     }
