026f0d5bf85294e9656eadd7df45265b0c4e5478
Elliott Zwertvaegher <Elliott Zwertvaegher>
2016-11-07 14:46:48 +0900

    Modified menu

--- CerealSquad/GameWorld/AWorld.cs
+++ CerealSquad/GameWorld/AWorld.cs
@@ -34,14 +34,6 @@ namespace CerealSquad.GameWorld
         public void Draw(RenderTarget target, RenderStates states)
         {
             Rooms.ForEach((ARoom room) => {
-                /*RenderTexture roomTexture = new RenderTexture(room.Size.Width * TILE_SIZE, room.Size.Height * TILE_SIZE);
-                room.Draw(roomTexture, states);
-                Sprite roomSprite = new Sprite(roomTexture.Texture);
-
-                roomSprite.Position = new SFML.System.Vector2f(room.Position.X * TILE_SIZE, room.Position.Y * TILE_SIZE);
-                target.Draw(roomSprite, states);
-                roomSprite.Dispose();
-                roomTexture.Dispose();*/
                 target.Draw(room, states);
             });
         }
