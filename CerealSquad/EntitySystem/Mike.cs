041251c90754feaae332561100e63a21a22c9ecc
Pierre <pierre.noel@epitech.eu>
2016-11-21 09:27:12 +0100

    Adding player

--- CerealSquad/EntitySystem/Mike.cs
+++ CerealSquad/EntitySystem/Mike.cs
@@ -17,6 +17,7 @@ namespace CerealSquad
             _ressources = new EntityResources();
 
             Factories.TextureFactory.Instance.load("MikeWalking", "Assets/Character/MikeWalking.png");
+            Factories.TextureFactory.Instance.load("MikeDying", "Assets/Character/Death/MikeDying.png");
             _ressources.InitializationAnimatedSprite(new Vector2u(64, 64));
 
             ((AnimatedSprite)_ressources.sprite).addAnimation((uint)EStateEntity.IDLE, "MikeWalking", new List<uint> { 0, 1 }, new Vector2u(128, 128));
@@ -24,7 +25,7 @@ namespace CerealSquad
             ((AnimatedSprite)_ressources.sprite).addAnimation((uint)EStateEntity.WALKING_LEFT, "MikeWalking", new List<uint> { 6, 7 }, new Vector2u(128, 128));
             ((AnimatedSprite)_ressources.sprite).addAnimation((uint)EStateEntity.WALKING_RIGHT, "MikeWalking", new List<uint> { 4, 5 }, new Vector2u(128, 128));
             ((AnimatedSprite)_ressources.sprite).addAnimation((uint)EStateEntity.WALKING_UP, "MikeWalking", new List<uint> { 2, 3 }, new Vector2u(128, 128));
-            //((AnimatedSprite)_ressources.sprite).addAnimation(EStateEntity.DYING, "JackWalking", new List<uint> { 12, 13, 14 }, new Vector2u(64, 64));
+            ((AnimatedSprite)_ressources.sprite).addAnimation((uint)EStateEntity.DYING, "MikeDying", Enumerable.Range(0, 7).Select(i => (uint)i).ToList(), new Vector2u(128, 128), 120);
 
             _ressources.CollisionBox = new FloatRect(new Vector2f(20.0f, -20.0f), new Vector2f(20.0f, 28.0f));
             _ressources.HitBox = new FloatRect(new Vector2f(20.0f, 26.0f), new Vector2f(20.0f, 28.0f));
--- CerealSquad/Program.cs
+++ CerealSquad/Program.cs
@@ -60,6 +60,11 @@ namespace CerealSquad
             awaiter.Add(ftpDownloader.RequireFile("CoffeeMachineThrowing", "Assets/Enemies/Boss/CoffeeMachineThrowingCoffee.png", new Uri(Downloaders.FTPDownloader.FTP_PATH + "Assets/Enemies/Boss/CoffeeMachineThrowingCoffee.png"), false));
             awaiter.Add(ftpDownloader.RequireFile("Rice", "Assets/Enemies/Normal/Rice.png", new Uri(Downloaders.FTPDownloader.FTP_PATH + "Assets/Enemies/Normal/Rice.png"), false));
 
+            awaiter.Add(ftpDownloader.RequireFile("Death_Hina", "Assets/Character/Death/HinaDying.png", new Uri(Downloaders.FTPDownloader.FTP_PATH + "Assets/Character/Death/HinaDying.png"), false));
+            awaiter.Add(ftpDownloader.RequireFile("Death_Jack", "Assets/Character/Death/JackDying.png", new Uri(Downloaders.FTPDownloader.FTP_PATH + "Assets/Character/Death/JackDying.png"), false));
+            awaiter.Add(ftpDownloader.RequireFile("Death_Mike", "Assets/Character/Death/MikeDying.png", new Uri(Downloaders.FTPDownloader.FTP_PATH + "Assets/Character/Death/MikeDying.png"), false));
+            awaiter.Add(ftpDownloader.RequireFile("Death_Tchong", "Assets/Character/Death/TchongDying.png", new Uri(Downloaders.FTPDownloader.FTP_PATH + "Assets/Character/Death/TchongDying.png"), false));
+
             awaiter.Add(ftpDownloader.RequireFile("jackHunter", "Assets/Character/JackHunter.png", new Uri(Downloaders.FTPDownloader.FTP_PATH + "Assets/Character/JackHunter.png"), false));
             awaiter.Add(ftpDownloader.RequireFile("orangina", "Assets/Character/Orangina.png", new Uri(Downloaders.FTPDownloader.FTP_PATH + "Assets/Character/Orangina.png"), false));
             awaiter.Add(ftpDownloader.RequireFile("basicEnnemy", "Assets/Character/BasicEnnemy.png", new Uri(Downloaders.FTPDownloader.FTP_PATH + "Assets/Character/BasicEnnemy.png"), false));
